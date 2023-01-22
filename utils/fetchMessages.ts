d1bcbe50e7278cc4f43d4744cdc9eca8580e2334
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-01-22 23:06:32 +0500

    created api for fetching message.

---
+++ utils/fetchMessages.ts
@@ -0,0 +1,14 @@
+import { Message } from "../typings";
+
+
+
+
+const fetcher = async () => {
+    const res = await fetch('/api/messages');
+    const data = await res.json();
+    const messages:Message[]= data.messages;
+
+    return messages;
+}
+
+export default fetcher;
