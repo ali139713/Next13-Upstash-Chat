43b57e3e32f4d90dd146ec01c1f39ca87b8bc9af
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-01-22 23:06:51 +0500

    display messages from server.

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client";
+
+import { Message } from "../typings";
+import fetcher from "../utils/fetchMessages"; 
+import useSWR from 'swr';
 
 const MessageList = () => {
+  const { data: messages, error, mutate } = useSWR<Message[]>("/api/messages", fetcher);
   return (
-    <main>MessageList</main>
-  )
-}
+    <main>
+      {messages?.map((message: Message) => (
+        <div key={message.id}>
+          <p>{message.message}</p>
+        </div>
+      ))}
+    </main>
+  );
+};
 
-export default MessageList
+export default MessageList;
