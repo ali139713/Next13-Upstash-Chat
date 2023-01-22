ae936e835cf9d97af90a50b8570b6e816452c110
Ali Azhar <aliazhar@Alis-MacBook-Air.local>
2023-01-22 21:14:51 +0500

    completed styling for text input.

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatInput from './ChatInput'
+import MessageList from './MessageList'
+
+const HomePage = () => {
+  return (
+    <>
+   <MessageList/>
+   <ChatInput/>
+   </>
+  )
+}
+
+export default HomePage
